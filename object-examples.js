720021e3ab6351b06b460d6144645493f05f2da9
Carly Collums <carlycollums@gmail.com>
2021-07-20 20:24:14 -0600

    Fix syntax

--- object-examples.js
+++ object-examples.js
@@ -1,17 +1,17 @@
 var car = {
-  type: "Mazda"
-  year: 2012
+  type: "Mazda",
+  year: 2012,
   color: ["Black", "White", "Grey"]
 }
 
 var animal = {
-  type: "Tiger"
-  age: 5
+  type: "Tiger",
+  age: 5,
   type: ["Bengal", "White", "Asian"]
 }
 
 var customer = {
-  name: "Carly"
-  age: 27
+  name: "Carly",
+  age: 27,
   isLoyaltyMember: true
 }
